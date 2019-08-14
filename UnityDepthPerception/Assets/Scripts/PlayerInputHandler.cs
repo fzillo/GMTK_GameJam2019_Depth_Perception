6e86ae815d9fb482864a487adb8a4a0763019178
Fabian Zillgens <fabian.zillgens@gmail.com>
2019-08-14 20:37:13 +0200

    PlayerInput + Trophy

---
+++ UnityDepthPerception/Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    //public Transform actor;
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    //public bool rbMovement = true;
+
+    public float movespeed = 20f;
+    public bool movable= true;
+
+    private Vector2 m_moveVect = new Vector2();
+
+    void Update()
+    {
+
+        m_moveVect.x = Input.GetAxisRaw("Horizontal");
+        m_moveVect.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.Space)){
+            animator.SetBool("SpacePressed", true);
+            m_moveVect = Vector2.zero;
+        } else
+        {
+            animator.SetBool("SpacePressed", false);
+        }
+
+        animator.SetFloat("Speed", m_moveVect.sqrMagnitude);
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("PlayerHitting"))
+        {
+            Debug.Log("Hitting");
+            movable = false;
+        } else
+        {
+            movable = true;
+        }
+
+        if (movable)
+        {
+            //actor.Translate(m_moveVect * Time.deltaTime * movespeed);
+           
+            rb.MovePosition(Vector2Int.RoundToInt(rb.position + m_moveVect * Time.deltaTime * movespeed));
+        }
+    }
+}
---
+++ UnityDepthPerception/Assets/Scripts/Trophy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trophy : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public Sprite brokenTrophy;
+
+    private bool m_broken = false;
+    private bool m_checkAnimEnd = false;
+
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!m_broken) { 
+            animator.SetTrigger("DoBreak");
+            m_broken = true;
+            m_checkAnimEnd = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {        
+        if (m_checkAnimEnd && animator.GetCurrentAnimatorStateInfo(0).IsName("TrophyBroken"))
+        {
+            Debug.Log(this.gameObject.name + " broke!");
+            animator.enabled = false;
+            m_checkAnimEnd = false;
+        }
+    }
+
+}
